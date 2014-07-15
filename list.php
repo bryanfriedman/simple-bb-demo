5c4d119aeee79671215a98e78c7e0e5027689a5a
Bryan Friedman <bryan.friedman@centurylinkcloud.com>
2014-07-15 15:48:30 -0700

    Add table to list.php.

--- list.php
+++ list.php
@@ -7,17 +7,45 @@ $con=mysqli_connect("localhost","bb-user","bb-password","simple-bb-demo");
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
+?>
 
-$result = mysqli_query($con,"SELECT * FROM main-bb");
+<table border='1'>
+<tr>
+<th>ID</th>
+<th>Name</th>
+<th>E-mail</th>
+<th>Comment</th>
+</tr>
+
+<?php
+$result = mysqli_query($con,"SELECT * FROM `main-bb`");
 if ( false===$result ) {
-  printf("error: %s\n", mysqli_error($con));
+  echo "Query failed: " . mysqli_connect_error($con);
+}
+else {
+
+   while($row = mysqli_fetch_array($result)) {
+     echo "<tr>";
+     echo "<td>" . $row['id'] . "</td>";
+     echo "<td>" . $row['name'] . "</td>";
+     echo "<td>" . $row['email'] . "</td>";
+     echo "<td>" . $row['comment'] . "</td>";
+     echo "</tr>";
+   }
+
 }
 
+?>
 
-while($row = mysqli_fetch_array($result)) {
-  echo $row['id'] . " - " . $row['name'] . " - " . $row['email'] . " - " . $row['comment'];
-  echo "<br>";
-}
+<tr>
+<td>ID</td>
+<td><input type="text" name="name" id="name" /></td>
+<td><input type="text" name="email" id="email" /></td>
+<td><textarea name="comment" id="comment"></textarea></td>
+</tr>
 
+</table>
+
+<?php
 mysqli_close($con);
 ?>
