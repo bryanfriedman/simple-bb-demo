9177097dd5c2b9bc15ad63bd9bda10871ec306ff
Bryan Friedman <bryan.friedman@centurylinkcloud.com>
2014-07-15 15:33:53 -0700

    Add list.php.

---
+++ list.php
@@ -0,0 +1,16 @@
+<?php
+$con=mysqli_connect("localhost","bb-user","bb-password","simple-bb-demo");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM main-bb");
+
+while($row = mysqli_fetch_array($result)) {
+  echo $row['id'] . " - " . $row['name'] . " - " . $row['email'] . " - " . $row['comment'];
+  echo "<br>";
+}
+
+mysqli_close($con);
+?>
